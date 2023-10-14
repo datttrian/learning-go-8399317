e72f75528e13239e937a644551b85d048f44f609
datttrian <datttrian@gmail.com>
2023-10-14 01:21:34 +0000

    Use math operators

--- practice/main.go
+++ practice/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"math"
 )
 
 func main() {
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ")
-	input, _ := reader.ReadString('\n')
-	fmt.Println("You entered:", input)
+	i1, i2, i3 := 12, 45, 68
+	intSum := i1 + i2 + i3
+	fmt.Println("Integer sum:", intSum)
+
+	f1, f2, f3 := 23.5, 65.1, 76.3
+	floatSum := f1 + f2 + f3
+	fmt.Println("Float sum:", floatSum)
+
+	roundedSum := math.Round(floatSum)
+	fmt.Println("Rounded sum:", roundedSum)
 
 }
