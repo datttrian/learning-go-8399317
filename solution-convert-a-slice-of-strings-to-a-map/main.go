6f4c4cf9cf2f9009062224622bc128a66f76d9bc
datttrian <datttrian@gmail.com>
2023-10-16 05:52:57 +0000

    Solution: Convert a slice of strings to a map

---
+++ solution-convert-a-slice-of-strings-to-a-map/main.go
@@ -0,0 +1,19 @@
+package main
+
+// Write your answer here, and then test your code.
+// Change these boolean values to control whether you see
+// the expected answer and/or hints.
+const showExpectedResult = false
+const showHints = false
+
+// Converts a slice of strings to a map object.
+func convertToMap(items []string) map[string]float64 {
+	// Create a map object
+	result := make(map[string]float64)
+	// Your code goes here
+	elementValue := 100 / float64(len(items))
+	for _, fruit := range items {
+		result[fruit] = elementValue
+	}
+	return result
+}
---
+++ solution-convert-a-slice-of-strings-to-a-map/test.go
@@ -0,0 +1,5 @@
+// This is how your code will be called.
+// Your answer should be a map object. Each of the map's elements should have a key typed as a string and an associated value that's a float64 number
+// You can edit this code to try different testing cases.
+slice := []string{"apple", "banana", "orange", "date"}
+result := convertToMap(slice)
