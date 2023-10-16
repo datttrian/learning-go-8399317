15efa744aaff1c5ddacec949b998985d62c6bd17
datttrian <datttrian@gmail.com>
2023-10-16 06:51:53 +0000

    Solution: Create a more advanced calculator app

---
+++ solution-create-a-more-advanced-calculator-app/test.go
@@ -0,0 +1,6 @@
+// This is how your code will be called.
+// You can edit this code to try different testing cases.
+value1 := "10"
+value2 := "5.5"
+operation := "+"
+result := calculate(value1, value2, operation)
